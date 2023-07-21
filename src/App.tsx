193eed0c7917480259bbf6ce401900b8ca8d3dfd
thomasop <tdss33@hotmail.com>
2023-07-21 18:07:12 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./global.css";
+import Spline from "@splinetool/react-spline";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+        <div style={{ width: "300px", height: "300px" }}>
+        <Spline scene="https://prod.spline.design/F2mvwDTtOCAaSwPi/scene.splinecode" />
+
+        </div>
+    </>
   );
-}
+};
 
 export default App;
