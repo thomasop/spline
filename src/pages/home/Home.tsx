897d9b70027fc16ef3fdf3154a0fa4113625452e
thomasop <tdss33@hotmail.com>
2023-07-24 10:01:28 +0200

    Edit

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,3 @@
-import { useDispatch, useSelector } from "react-redux";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
-import "../../global.css";
-import styles from "./Home.module.scss";
-import { motion, useScroll, useSpring } from "framer-motion";
-import WorkPhp from "../../components/work/php/WorkPhp";
-import WorkPerso from "../../components/work/perso/WorkPerso";
-import WorkClient from "../../components/work/client/WorkClient";
-import WorkReact from "../../components/work/react/WorkReact";
-import Contact from "../../components/contact/Contact";
 import Spline from "@splinetool/react-spline";
 
 const App = () => {
